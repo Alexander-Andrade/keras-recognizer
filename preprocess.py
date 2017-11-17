b56a1febe505286fc76f467cd1b215d473072a1d
Alexander-Andrade <veimar.94@gmail.com>
2017-11-17 18:57:07 +0300

    image cropping

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageMath
 n_cols = 3
 n_rows = 3
 
+
 def file_names(path):
     return ( f for f in listdir(path) if isfile(join(path, f)) )
 
@@ -13,6 +14,11 @@ def file_names(path):
 def change_filename_ext(filename, new_ext):
     name, ext = path.splitext(filename)
     return name + '.' + new_ext
+
+
+def gen_image_part_name(image_name, num, new_ext):
+    name, ext = path.splitext(image_name)
+    return name + str(num) + '.' + new_ext
 
 
 def tile_traverse_gen(width, height, rows, cols):
@@ -47,19 +53,5 @@ for image_name in file_names('train_google_earth/preprocess\\images'):
     masked_im = apply_mask(im, mask)
     width, height = masked_im.size
     for i, box in enumerate(tile_traverse_gen(width, height, n_rows, n_cols)):
-        im_part_name = change_filename_ext(image_name+str(i), 'png')
+        im_part_name = gen_image_part_name(image_name, i, 'png')
         masked_im.crop(box).save(im_part_name, 'PNG')
-
-# im = Image.open('train_google_earth/preprocess\\images\\22828930_15.tiff').convert('RGB')
-# map = Image.open('train_google_earth/preprocess\\target_maps\\22828930_15.tif').convert('RGB')
-#
-# print(im.format, im.size, im.mode)
-# print(map.format, map.size, map.mode)
-#
-# r, g, b = map.split()
-# new_map = Image.merge("RGBA", (r, r, r, r))
-# new_map.show()
-#
-# masked = Image.composite(im, new_map, mask=new_map).convert('RGB')
-# print(masked.format, masked.size, masked.mode)
-# masked.show()
