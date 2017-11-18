447cef78734e522c80adeef2cb202f8c534dfc66
Alexander-Andrade <veimar.94@gmail.com>
2017-11-18 21:03:31 +0300

    detect and filter contours

--- recognize.py
+++ recognize.py
@@ -5,6 +5,7 @@ import samples_utils
 import numpy as np
 import cv2 as cv
 from PIL import Image, ImageMath
+import itertools
 
 test_path = 'train_google_earth/test'
 
@@ -30,13 +31,23 @@ p_im = Image.open(test_path + '/' + file_name)
 print("pillow format: {}, {}, {}".format(p_im.format, p_im.size, p_im.mode))
 
 edges = cv.Canny(im, 100, 200)
-cv.imshow('edges', edges)
-cv.imshow('im', im)
+# cv.imshow('edges', edges)
+# cv.imshow('im', im)
 
 im2, contours, hierarchy = cv.findContours(edges, 1, 2)
-cv.drawContours(im, contours, -1, (0,255,0), 1)
+contours = list(filter(lambda c: cv.contourArea(c) > 200, contours))
+# contours = list(filter(lambda c: cv.arcLength(c, False) > 100, contours))
+# contours = list(map(lambda c: cv.approxPolyDP(c, 3.0, True), contours))
 
+cv.drawContours(im, contours, -1, (0,255,0), 1)
 cv.imshow('im_with_contours', im)
+
+
+bounding_rects = list(map(lambda c: cv.boundingRect(c), contours))
+for x, y, w, h in bounding_rects:
+    cv.rectangle(im, (x, y), (x+w, y+h), (0, 0, 255), 1)
+
+cv.imshow('im_with_rects', im)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
